a61337959fc62bb2fc2f19bb720fc1b213e4ae9e
Kenia <keniavalladarez@gmail.com>
2018-07-09 17:07:54 -0400

    Changes region for demo-2.

--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -1,10 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
+
 variable "AMIS" {
   type = "map"
+
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
@@ -15,9 +18,11 @@ variable "AMIS" {
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
+
 variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
+
 variable "INSTANCE_USERNAME" {
   default = "ubuntu"
 }
